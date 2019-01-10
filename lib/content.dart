7f0bb12d3b9df85a7ab6edf52760eecbb0c66415
Steve Alexander <steve@stevea.com>
2019-01-10 15:07:14 +0000

    further simplifications while still reproducing the problem

--- lib/content.dart
+++ lib/content.dart
@@ -1,29 +1,20 @@
+import 'package:flutter/foundation.dart' show compute;
+
+Future<Content> parse() async {
+  return await compute<String, Content>(_parse, null);
+}
+
+Content _parse(String json) {
+  return Content(PartOfDay.afternoon);
+}
+
 class Content {
   Content(partOfDay) : this.partOfDay = Set()..add(partOfDay);
   final Set<PartOfDay> partOfDay;
-
-  static Content fromJson(struct) {
-    PartOfDay partOfDay = parsePartOfDay(struct['part_of_day']);
-    return Content(partOfDay);
-  }
 }
-
 
 enum PartOfDay {
   morning,
   afternoon,
   evening
-}
-
-PartOfDay parsePartOfDay(String partOfDayString) {
-  switch (partOfDayString) {
-    case 'morning':
-      return PartOfDay.morning;
-    case 'afternoon':
-      return PartOfDay.afternoon;
-    case 'evening':
-      return PartOfDay.evening;
-    default:
-      throw Exception('Bad PartOfDay: <$partOfDayString>');
-  }
 }
